370dd35892addee1f15de7776acc9d544658d66b
Himanshu54 <himu5493@gmail.com>
2018-08-02 17:44:42 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 0, value: 0 },
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 }
+    ]
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const idx = counters.indexOf(counter);
+    counters[idx] = { ...counter };
+    counters[idx].value++;
+    this.setState({ counters });
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState(counters);
+  };
+  handleDelete = counterId => {
+    // console.log("event handler callled", counterId);
+    const counters = this.state.counters.filter(c => c.id != counterId);
+    this.setState({ counters });
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <React.Fragment>
+        <NavBar
+          totalCounter={this.state.counters.filter(c => c.value > 0).length}
+        />
+        <main className="containers">
+          <Counters
+            onDelete={this.handleDelete}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            counters={this.state.counters}
+          />
+        </main>
+      </React.Fragment>
     );
   }
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
-import Counters from "./components/counters";
 class NavBar extends Component {
+  state = {};
   render() {
     return (
-      <nav className="navbar navbar-light bg-light">
-        <span className="navbar-brand mb-0 h1">Navbar</span>
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary ">
+            {this.props.totalCounter}
+          </span>
+        </a>
       </nav>
     );
   }
